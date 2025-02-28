b2a94f022544762cdec34828ee6a9c14cc88be35
ako <akakimikaia@gmail.com>
2025-02-28 14:02:14 +0100

    chore: resolve ts error with imports

--- svelte.config.js
+++ svelte.config.js
@@ -11,10 +11,11 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-    alias: {
-      $utils: "src/utils",
-      $types: "src/types",
-    }
+		alias: {
+			$lib: 'src/lib',
+			$utils: 'src/utils',
+			$types: 'src/types'
+		}
 	}
 };
 export default config;
